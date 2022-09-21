cb3c8a98184afdb28f8bf7a15468aa9dba20d644
Dongkwan Kim <todoaskit@gmail.com>
2022-09-21 23:40:21 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip3 install torch==1.9.0
+pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html
+pip3 install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.9.0+cu102.html  # See https://github.com/rusty1s/pytorch_sparse/issues/207
+pip3 install torch-geometric==1.7.2
+pip3 install optuna
